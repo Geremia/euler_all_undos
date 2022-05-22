89c06bb3a6ce0cfd4f0050bd4b2a0fd7e7f977df
Geremia <Geremia@users.noreply.github.com>
2022-05-21 20:07:56 -0700

    undo-commit166

--- euler2.py
+++ euler2.py
@@ -17,7 +17,7 @@ def eulerCycle(G, start): # G = graph (dictionary)
                 path.append(neighbor)
                 if G[n] == []:
                     G.pop(n)
-                    n = list(.keys())[0]
+                    n = list(G.keys())[0]
                 else:
                     n = neighbor
                 break
