8d3d233ce83dfa5314a6996a2a8b83bf0b83d1f5
Geremia <Geremia@users.noreply.github.com>
2022-05-21 19:12:10 -0700

    undo-commit54

--- euler2.py
+++ euler2.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
 
+def eulerCycle(G, start): # G = graph (dictionary)
+  path = [start]
+  #construct a set of all edges in graph:
+  edgesRemaining = set()
+  for n in G.keys():
+    for neighbor in G[n]:  # neighbor is a neighbor of node.
+      edgesRemaining.add((n,neighbor))
+  n = start
+    while True:
+    neighbor = G[n]
+    edge = (n,neighbor)
+    if edge in edgesRemaining:
+      edgesRemaining.remove(edge)
+      path.append(n)
+    if len(edgesRemaining)==0:
+      return path
+    n = neighbor
+    
 
 eulerCycle({0:[1],1:[0,2],2:[1,0]}, 0)
