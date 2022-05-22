dffc7c722b30f6f3e9cd4c7a7d172eaa4791f8c1
Geremia <Geremia@users.noreply.github.com>
2022-05-21 19:26:17 -0700

    undo-commit88

--- euler2.py
+++ euler2.py
@@ -13,7 +13,7 @@ def eulerCycle(G, start): # G = graph (dictionary)
             edge = (n,neighbor)
         if edge in edgesRemaining:
             edgesRemaining.remove(edge)
-          path.append(n)
+            path.append(n)
           break
       n = neighbor
     return 0
