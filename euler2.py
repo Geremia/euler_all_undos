d7abf60d4d2b5073960809b9371e7a9b8097c8d1
Geremia <Geremia@users.noreply.github.com>
2022-05-21 19:28:40 -0700

    undo-commit97

--- euler2.py
+++ euler2.py
@@ -10,6 +10,7 @@ def eulerCycle(G, start): # G = graph (dictionary)
     n = start
     while len(edgesRemaining)!=0:
         for neighbor in G[n]:
+            print(neighbor)
             edge = (n,neighbor)
         if edge in edgesRemaining:
             edgesRemaining.remove(edge)
