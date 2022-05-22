eb8de8d1fc644ef359a0ad1ae3a6e7d7d11a5598
Geremia <Geremia@users.noreply.github.com>
2022-05-21 19:13:25 -0700

    undo-commit66

--- euler2.py
+++ euler2.py
@@ -4,7 +4,7 @@ def eulerCycle(G, start): # G = graph (dictionary)
     path = [start]
     #construct a set of all edges in graph:
     edgesRemaining = set()
-  for n in G.keys():
+    for n in G.keys():
       for neighbor in G[n]:  # neighbor is a neighbor of node.
           edgesRemaining.add((n,neighbor))
   n = start
