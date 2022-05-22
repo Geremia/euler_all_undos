0736e7c1801af56d82576370332079b1c0ec5a19
Geremia <Geremia@users.noreply.github.com>
2022-05-21 19:40:27 -0700

    undo-commit128

--- euler2.py
+++ euler2.py
@@ -19,4 +19,4 @@ def eulerCycle(G, start): # G = graph (dictionary)
         print(neighbor, edgesRemaining,path)
     return path
 
-print(eulerCycle({0:[1],1:[0,2],2:[1,0]}, 0))
+print(eulerCycle({0:[1],1:[0,2],2:[1,0]}, 1 ))
