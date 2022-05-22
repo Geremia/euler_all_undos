428dcfd387db86706f4250fcabd5ceb45e0af369
Geremia <Geremia@users.noreply.github.com>
2022-05-21 19:37:13 -0700

    undo-commit113

--- euler2.py
+++ euler2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def eulerCycle(G, start): # G = graph (dictionary)
-    path = []
+    path = [start]
     #construct a set of all edges in graph:
     edgesRemaining = set()
     for n in G.keys():
